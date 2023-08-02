fd1d8215dbe11731260177e533c96653e789bdbc
Damola Olusakin <olusakinadedamola@gmail.com>
2023-08-02 11:08:47 -0400

    configured tailwind

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,1 +1,15 @@
 
+
+function SignupPage() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">Name</label>
+        <input type="text" placeholder="Enter your name" className="border-b-2 border-blue-100 ml-5 rounded-md"/>
+      </form>
+    </div>
+
+  )
+}
+
+export default SignupPage
