e13e678a6e5e8ebf57014b509198f6c0a1a132f9
Damola Olusakin <olusakinadedamola@gmail.com>
2023-08-02 21:31:32 -0400

    created add pet modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,40 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import userService from "./utils/userService";
 
+import HomePage from "./pages/HomePage/HomePage";
+import AddJob from "./components/modal/AddJob/AddJob";
+import AddPet from "./components/modal/AddPet/AddPet";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignupPage from "./pages/SignupPage/SignupPage";
-import userService from "./utils/userService";
+
 
 function App() {
   const [user, setUser] = useState(userService.getUser())
+  const [showPetModal, setShowPetModal] = useState(true);
+  const [showJobModal, setShowJobModal] = useState(false);
 
   function handleSignUpOrLogin(){
     setUser(userService.getUser())
   }
 
+  function handlePetModalSkip(){
+    setShowPetModal(false)
+    setShowJobModal(true)
+  }
+
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home Pageeeeeeeeeee</h1>} />
+    <div>
+      {showPetModal ? <AddPet handlePetModalSkip={handlePetModalSkip} /> : null}
+      {showJobModal ? <AddJob /> : null}
+      <Routes>
+      <Route path="/" element={<HomePage/>} />
       <Route path="/login" element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />} />
       <Route path="/signup" element={<SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />} />
     </Routes>
+    </div>
+    
   );
 }
 
