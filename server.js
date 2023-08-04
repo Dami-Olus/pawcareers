4bc68b908b7702b84543024473cf574a17b6ca55
Damola Olusakin <olusakinadedamola@gmail.com>
2023-08-04 14:29:17 -0400

    setup routes, controller, api for posts

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use("/api/users", require("./routes/api/users"));
 
 app.use('/api/pets', require("./routes/api/pets"));
 app.use('/api/jobs', require("./routes/api/jobs"));
+app.use('/api/posts', require("./routes/api/posts"));
 
 // "catch all" route
 app.get('/*', function(req, res) {
--- src/components/PostGallery/PostGallery.jsx
+++ src/components/PostGallery/PostGallery.jsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import PostCard from "../PostCard/PostCard";
 
 function PostGallery() {
+  const [post, setPost ] = useState({})
+  const [selectedFile, setSelectedFile] = useState()
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append()
+  }
+
+  function handleFileInput(e){
+    setSelectedFile(e.target.files[0])
+  }
+
+  function handleChange(e){
+setPost({
+  ...post,
+  [e.target.name]: e.target.value
+})
+  }
+  
   return (
     <div className="text-[#407bff]  mt-20 ml-10 mr-10  rounded-md  flex flex-col items-center p-8 flex-[0.6]">
       <div>
         <form action="" className="flex flex-col gap-2 mb-5">
           <input type="file" />
-          <textarea name="postContent" id="" cols="30" rows="2" placeholder="what's on your mind" className="outline-none border-2 border-[#407bff] rounded-md"></textarea>
+          <textarea name="caption" id="" cols="30" rows="2" placeholder="what's on your mind" className="outline-none border-2 border-[#407bff] rounded-md"></textarea>
           <button className="text-white bg-[#407BFF] py-2 rounded-md w-32 ">Post</button>
         </form>
       </div>
       {/* Postcard */}
-      <div className="bg-green-200 h-[400px] w-[400px] flex flex-col justify-center mb-5">
-        <p>Post content</p>
-      </div>
-
-      <div className="bg-green-200 h-[400px] w-[400px] flex flex-col justify-center mb-5">
-        <p>Post content</p>
-      </div>
-
-      <div className="bg-green-200 h-[400px] w-[400px] flex flex-col justify-center mb-5">
-        <p>Post content</p>
-      </div>
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
     </div>
   );
 }
